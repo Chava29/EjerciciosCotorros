e1d253d394a0b3f9e5accd6334a9206963b8329b
Chava29 <salvita2013@gmail.com>
2025-08-20 03:57:01 -0600

    Ejercicio Mercería (ifs aninados)

--- src/main/java/Highschool.java
+++ src/main/java/Highschool.java
@@ -31,6 +31,6 @@ public class Highschool
                 }
             }
         }
-        System.out.println("--------------------------------------------");
+        System.out.println("-------------------------------------------");
     }
 }
